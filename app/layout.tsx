7d4e52bee7b8441edf7bfeef2a6795cf5f6ba654
Chirag Parmar <95179516+chiragxp@users.noreply.github.com>
2025-03-22 19:57:06 -0400

    Implemented sign-in and sign-up pages and routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "sonner";
+
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
@@ -19,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${monaSans.className} antialiased`}>{children}</body>
+      <body className={`${monaSans.className} antialiased pattern`}>
+        {children}
+
+        <Toaster />
+      </body>
     </html>
   );
 }
