daefd19dd7b3cbf5dfc6042d18b467c8ccd04563
Chirag Parmar <95179516+chiragxp@users.noreply.github.com>
2025-03-23 00:48:39 -0400

    Firebase setup done

---
+++ firebase/client.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMSh59GGwvZT7h9MORKIjzuknnq4DHwZU",
+  authDomain: "prepwise-fec21.firebaseapp.com",
+  projectId: "prepwise-fec21",
+  storageBucket: "prepwise-fec21.firebasestorage.app",
+  messagingSenderId: "335027424307",
+  appId: "1:335027424307:web:77bda17deb355fa5522746",
+  measurementId: "G-L5S9S2NNVG",
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
